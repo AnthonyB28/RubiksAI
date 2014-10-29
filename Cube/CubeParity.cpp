cc9da2a937010d820a39e19e1f67efa77832e0bb
anthonyb28 <anthony@lssclan.org>
2014-10-29 00:26:28 -0400

    Missed CubeParity
    Github somehow missed the CubeParity.cpp

--- Cube/CubeParity.cpp
+++ Cube/CubeParity.cpp
@@ -5,8 +5,8 @@ namespace Rubiks
 	// Do our 3 tests to find the 1/12 combinations that can be incorrect
 	bool Cube::CheckValidParity()
 	{
-		UInt32 ** cornerCubies = GetCornerCubies();
-		UInt32 ** edgeCubies = GetEdgeCubies();
+		CubieCollection cornerCubies = GetCornerCubies();
+		CubieCollection edgeCubies = GetEdgeCubies();
 		bool isValid = true;
 
 		if (!CheckPermutations(cornerCubies, edgeCubies) ||
@@ -16,14 +16,11 @@ namespace Rubiks
 			isValid = false;
 		}
 
-		DeleteCornerCubies(cornerCubies);
-		DeleteEdgeCubies(edgeCubies);
-
 		return isValid;
 	}
 
 	// Checks for permutation parity
-	bool Cube::CheckPermutations(UInt32 ** cornerCubies, UInt32 ** edgeCubies)
+	bool Cube::CheckPermutations(CubieCollection& cornerCubies, CubieCollection& edgeCubies)
 	{
 		int edgePositions[12];
 		// Check number of edge swaps
@@ -90,7 +87,7 @@ namespace Rubiks
 	// RGW - RBW - RGY - RBY - GOW - GOY - YOB - BOW
 	// DON'T FORGET TO CALL DELETECORNERCUBIES FROM THIS!!
 	// TODO make this a class or something
-	Cube::UInt32 ** Cube::GetCornerCubies()
+	Cube::CubieCollection Cube::GetCornerCubies()
 	{
 		CornerCubies redCorners = m_Faces[0].GetCorners();
 		CornerCubies greenCorners = m_Faces[1].GetCorners();
@@ -98,33 +95,18 @@ namespace Rubiks
 		CornerCubies blueCorners = m_Faces[3].GetCorners();
 		CornerCubies orangeCorners = m_Faces[4].GetCorners();
 		CornerCubies whiteCorners = m_Faces[5].GetCorners();
-
-		UInt32 ** cornerCubies = new UInt32*[8];
-
-		// 8 corners should exist
-		cornerCubies[0] = new UInt32[3]{ redCorners.x, greenCorners.x, whiteCorners.z };
-		cornerCubies[1] = new UInt32[3]{ redCorners.y, blueCorners.y, whiteCorners.w };
-		cornerCubies[2] = new UInt32[3]{ redCorners.z, greenCorners.y, yellowCorners.x };
-		cornerCubies[3] = new UInt32[3]{ redCorners.w, blueCorners.x, yellowCorners.y };
-		cornerCubies[4] = new UInt32[3]{ greenCorners.z, orangeCorners.z, whiteCorners.x };
-		cornerCubies[5] = new UInt32[3]{ greenCorners.w, orangeCorners.x, yellowCorners.z };
-		cornerCubies[6] = new UInt32[3]{ blueCorners.z, orangeCorners.y, yellowCorners.w };
-		cornerCubies[7] = new UInt32[3]{ blueCorners.w, orangeCorners.w, whiteCorners.y };
-
-		return cornerCubies;
+		return CubieCollection{
+			std::vector < UInt32 > { redCorners.x, greenCorners.x, whiteCorners.z },
+			std::vector < UInt32 > { redCorners.y, blueCorners.y, whiteCorners.w },
+			std::vector < UInt32 > { redCorners.z, greenCorners.y, yellowCorners.x },
+			std::vector < UInt32 > { redCorners.w, blueCorners.x, yellowCorners.y },
+			std::vector < UInt32 > { greenCorners.z, orangeCorners.z, whiteCorners.x },
+			std::vector < UInt32 > { greenCorners.w, orangeCorners.x, yellowCorners.z },
+			std::vector < UInt32 > { blueCorners.z, orangeCorners.y, yellowCorners.w },
+			std::vector < UInt32 > { blueCorners.w, orangeCorners.w, whiteCorners.y }};
 	}
 
-	// Delete the corner cubies Collection
-	void Cube::DeleteCornerCubies(UInt32 ** cornerCubies)
-	{
-		for (int i = 0; i < 8; ++i)
-		{
-			delete[] cornerCubies[i];
-		}
-		delete[] cornerCubies;
-	}
-
-	int Cube::GetCornerPermutationValue(UInt32 cornerCubie[3])
+	int Cube::GetCornerPermutationValue(std::vector<UInt32>& cornerCubie)
 	{
 		UInt32 x = cornerCubie[0];
 		UInt32 y = cornerCubie[1];
@@ -214,7 +196,7 @@ namespace Rubiks
 	// correct orientation = 0
 	// clockwise = 1
 	// anti-clockwise = 2
-	int Cube::GetCornerOrientationValue(UInt32 cornerValues[3], int corner)
+	int Cube::GetCornerOrientationValue(std::vector<UInt32>& cornerValues, int corner)
 	{
 		for (int i = 0; i < 3; ++i)
 		{
@@ -250,7 +232,7 @@ namespace Rubiks
 	}
 
 	// Corner cubie parity test
-	bool Cube::CheckCornerParity(UInt32 ** cornerCubies)
+	bool Cube::CheckCornerParity(CubieCollection& cornerCubies)
 	{
 		int totalValue = 0;
 
@@ -272,7 +254,7 @@ namespace Rubiks
 
 	// Check to make sure corner colors are valid
 	// RGW - RBW - RGY - RBY - GOW - GOY - YOB - BOW
-	bool Cube::CheckValidCornerColors(UInt32 cornerCubie[3], int corner)
+	bool Cube::CheckValidCornerColors(std::vector<UInt32>& cornerCubie, int corner)
 	{
 		if (corner == 0 || corner == 3 || corner == 4 || corner == 6)
 		{
@@ -397,7 +379,7 @@ namespace Rubiks
 	// RW - RG - RB - RY - GW - GY - GO - YB - YO - BW - BO - OW
 	// DON'T FORGET TO CALL DELETEEDGECUBIES FROM THIS!!
 	// TODO make this a class or something
-	Cube::UInt32 ** Cube::GetEdgeCubies()
+	Cube::CubieCollection Cube::GetEdgeCubies()
 	{
 		EdgeCubies redEdges = m_Faces[0].GetEdges();
 		EdgeCubies greenEdges = m_Faces[1].GetEdges();
@@ -406,36 +388,23 @@ namespace Rubiks
 		EdgeCubies orangeEdges = m_Faces[4].GetEdges();
 		EdgeCubies whiteEdges = m_Faces[5].GetEdges();
 
-		UInt32 ** edgeCubies = new UInt32*[12];
-
-		// 8 corners should exist
-		edgeCubies[0] = new UInt32[2]{ redEdges.x, whiteEdges.w }; // RW
-		edgeCubies[1] = new UInt32[2]{ redEdges.y, greenEdges.x }; // RG
-		edgeCubies[2] = new UInt32[2]{ redEdges.z, blueEdges.x }; // RB
-		edgeCubies[3] = new UInt32[2]{ redEdges.w, yellowEdges.x }; // RY
-		edgeCubies[4] = new UInt32[2]{ greenEdges.y, whiteEdges.y }; // GW
-		edgeCubies[5] = new UInt32[2]{ greenEdges.z, yellowEdges.y }; // GY
-		edgeCubies[6] = new UInt32[2]{ greenEdges.w, orangeEdges.y }; // GO
-		edgeCubies[7] = new UInt32[2]{ yellowEdges.z, blueEdges.y }; // YB
-		edgeCubies[8] = new UInt32[2]{ yellowEdges.w, orangeEdges.x }; // YO
-		edgeCubies[9] = new UInt32[2]{ blueEdges.z, whiteEdges.z }; // BW
-		edgeCubies[10] = new UInt32[2]{ blueEdges.w, orangeEdges.z }; // BO
-		edgeCubies[11] = new UInt32[2]{ orangeEdges.w, whiteEdges.x }; // OW
-
-		return edgeCubies;
+		return CubieCollection{ 
+			{ redEdges.x, whiteEdges.w },
+			{ redEdges.y, greenEdges.x },
+			{ redEdges.z, blueEdges.x },
+			{ redEdges.w, yellowEdges.x },
+			{ greenEdges.y, whiteEdges.y },
+			{ greenEdges.z, yellowEdges.y },
+			{ greenEdges.w, orangeEdges.y },
+			{ yellowEdges.z, blueEdges.y },
+			{ yellowEdges.w, orangeEdges.x },
+			{ blueEdges.z, whiteEdges.z },
+			{ blueEdges.w, orangeEdges.z },
+			{ orangeEdges.w, whiteEdges.x }
+		};
 	}
 
-	// Delete the corner cubies Collection
-	void Cube::DeleteEdgeCubies(UInt32 ** edgeCubies)
-	{
-		for (int i = 0; i < 12; ++i)
-		{
-			delete[] edgeCubies[i];
-		}
-		delete[] edgeCubies;
-	}
-
-	int Cube::GetEdgePermutationValue(UInt32 edgeCubie[2])
+	int Cube::GetEdgePermutationValue(std::vector<UInt32>& edgeCubie)
 	{
 		UInt32 x = edgeCubie[0];
 		UInt32 y = edgeCubie[1];
@@ -508,7 +477,7 @@ namespace Rubiks
 	}
 
 	// Edge cubie parity test
-	bool Cube::CheckEdgeParity(UInt32 ** edgeCubies)
+	bool Cube::CheckEdgeParity(CubieCollection& edgeCubies)
 	{
 		// Value of all edge orientations is even
 		// LEFT RIGHT TOP AND BOTTOM flips zero edges
@@ -526,7 +495,7 @@ namespace Rubiks
 		int n = 0;
 		for (int i = 0; i < 12; ++i)
 		{
-			UInt32 edgeToValidate[2] = { edgeCubies[i][0], edgeCubies[i][1] };
+			std::vector<UInt32>& edgeToValidate = edgeCubies[i];
 			if (CheckValidEdgeColors(edgeToValidate))
 			{
 				if (i == 0) // RW
@@ -1160,7 +1129,7 @@ namespace Rubiks
 		}
 	}
 
-	int Cube::GetEdgeOrientationValue(UInt32 edgeCubie[2], int i)
+	int Cube::GetEdgeOrientationValue(std::vector<UInt32>& edgeCubie, int i)
 	{
 		if (i == 0) // RW
 		{
@@ -1779,21 +1748,21 @@ namespace Rubiks
 	}
 
 	// Edges are either one of two color combinations
-	bool Cube::CheckValidEdgeColors(UInt32 edge[2])
+	bool Cube::CheckValidEdgeColors(std::vector<UInt32>& edgeCubie)
 	{
 		if (
-			(edge[0] == RED && edge[1] == WHITE) || (edge[0] == WHITE && edge[1] == RED) ||
-			(edge[0] == RED && edge[1] == BLUE) || (edge[0] == BLUE && edge[1] == RED) ||
-			(edge[0] == RED && edge[1] == GREEN) || (edge[0] == GREEN && edge[1] == RED) ||
-			(edge[0] == RED && edge[1] == YELLOW) || (edge[0] == YELLOW && edge[1] == RED) ||
-			(edge[0] == GREEN && edge[1] == WHITE) || (edge[0] == WHITE && edge[1] == GREEN) ||
-			(edge[0] == GREEN && edge[1] == YELLOW) || (edge[0] == YELLOW && edge[1] == GREEN) ||
-			(edge[0] == GREEN && edge[1] == ORANGE) || (edge[0] == ORANGE && edge[1] == GREEN) ||
-			(edge[0] == YELLOW && edge[1] == BLUE) || (edge[0] == BLUE && edge[1] == YELLOW) ||
-			(edge[0] == YELLOW && edge[1] == ORANGE) || (edge[0] == ORANGE && edge[1] == YELLOW) ||
-			(edge[0] == BLUE && edge[1] == WHITE) || (edge[0] == WHITE && edge[1] == BLUE) ||
-			(edge[0] == BLUE && edge[1] == ORANGE) || (edge[0] == ORANGE && edge[1] == BLUE) ||
-			(edge[0] == ORANGE && edge[1] == WHITE) || (edge[0] == WHITE && edge[1] == ORANGE)
+			(edgeCubie[0] == RED && edgeCubie[1] == WHITE) || (edgeCubie[0] == WHITE && edgeCubie[1] == RED) ||
+			(edgeCubie[0] == RED && edgeCubie[1] == BLUE) || (edgeCubie[0] == BLUE && edgeCubie[1] == RED) ||
+			(edgeCubie[0] == RED && edgeCubie[1] == GREEN) || (edgeCubie[0] == GREEN && edgeCubie[1] == RED) ||
+			(edgeCubie[0] == RED && edgeCubie[1] == YELLOW) || (edgeCubie[0] == YELLOW && edgeCubie[1] == RED) ||
+			(edgeCubie[0] == GREEN && edgeCubie[1] == WHITE) || (edgeCubie[0] == WHITE && edgeCubie[1] == GREEN) ||
+			(edgeCubie[0] == GREEN && edgeCubie[1] == YELLOW) || (edgeCubie[0] == YELLOW && edgeCubie[1] == GREEN) ||
+			(edgeCubie[0] == GREEN && edgeCubie[1] == ORANGE) || (edgeCubie[0] == ORANGE && edgeCubie[1] == GREEN) ||
+			(edgeCubie[0] == YELLOW && edgeCubie[1] == BLUE) || (edgeCubie[0] == BLUE && edgeCubie[1] == YELLOW) ||
+			(edgeCubie[0] == YELLOW && edgeCubie[1] == ORANGE) || (edgeCubie[0] == ORANGE && edgeCubie[1] == YELLOW) ||
+			(edgeCubie[0] == BLUE && edgeCubie[1] == WHITE) || (edgeCubie[0] == WHITE && edgeCubie[1] == BLUE) ||
+			(edgeCubie[0] == BLUE && edgeCubie[1] == ORANGE) || (edgeCubie[0] == ORANGE && edgeCubie[1] == BLUE) ||
+			(edgeCubie[0] == ORANGE && edgeCubie[1] == WHITE) || (edgeCubie[0] == WHITE && edgeCubie[1] == ORANGE)
 			)
 		{
 			return true;
